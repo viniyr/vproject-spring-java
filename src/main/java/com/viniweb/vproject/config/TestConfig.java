75beacd10c2a396aa3031dcd7ee78cd4843409e0
viniyr <vinicius.yonezawa@gmail.com>
2021-07-27 00:10:31 -0300

    Product

--- src/main/java/com/viniweb/vproject/config/TestConfig.java
+++ src/main/java/com/viniweb/vproject/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.viniweb.vproject.entities.Category;
 import com.viniweb.vproject.entities.Order;
+import com.viniweb.vproject.entities.Product;
 import com.viniweb.vproject.entities.User;
 import com.viniweb.vproject.entities.enums.OrderStatus;
 import com.viniweb.vproject.repositories.CategoryRepository;
 import com.viniweb.vproject.repositories.OrderRepository;
+import com.viniweb.vproject.repositories.ProductRepository;
 import com.viniweb.vproject.repositories.UserRepository;
 
 @Configuration
@@ -29,12 +31,21 @@ public class TestConfig implements CommandLineRunner{
 	@Autowired
 	private CategoryRepository categoryRepository;
 	
+	@Autowired
+	private ProductRepository productRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
 		
 		Category cat1 = new Category(null, "Electronics"); 
 		Category cat2 = new Category(null, "Books"); 
 		Category cat3 = new Category(null, "Swords");
+		
+		Product p1 = new Product(null, "Night Sword", "Lorem ipsum dolor sit amet, consectetur.", 90.5, ""); 
+		Product p2 = new Product(null, "Enchiridion", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, ""); 
+		Product p3 = new Product(null, "Neptur", "Nam eleifend maximus tortor, at mollis.", 9999.0, ""); 
+		Product p4 = new Product(null, "Finn Sword", "Donec aliquet odio ac rhoncus cursus.", 1200.0, ""); 
+		Product p5 = new Product(null, "Adventure time, Vol. 1", "Cras fringilla convallis sem vel faucibus.", 400.96, "");
 		
 		User u1 = new User(null, "Finn Mertens", "finn@gmail.com", "999999999", "123456"); 
 		User u2 = new User(null, "Jake the Dog", "jake@gmail.com", "999999992", "123456");
@@ -45,6 +56,7 @@ public class TestConfig implements CommandLineRunner{
 		
 
 		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
 		
