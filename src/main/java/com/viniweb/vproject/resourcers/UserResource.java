cf370c64069f51246f15659aaae9933624dde1e7
viniyr <vinicius.yonezawa@gmail.com>
2021-07-26 22:45:43 -0300

    Service layer, component registration

--- src/main/java/com/viniweb/vproject/resourcers/UserResource.java
+++ src/main/java/com/viniweb/vproject/resourcers/UserResource.java
@@ -1,20 +1,34 @@
 package com.viniweb.vproject.resourcers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.viniweb.vproject.entities.User;
+import com.viniweb.vproject.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+	@Autowired
+	private UserService service;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll() { 
-		User u = new User(1L , "Amanda Ogoshi", "mand_ogoshi@hotmail.com", "99026-2852", "anya&atena");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll() { 
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id) { 
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj); 
 	}
 	
 }
