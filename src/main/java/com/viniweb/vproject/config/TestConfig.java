7540b057a5fe388b98bd85425a0eb06c20e788b3
viniyr <vinicius.yonezawa@gmail.com>
2021-07-26 23:53:03 -0300

    Category

--- src/main/java/com/viniweb/vproject/config/TestConfig.java
+++ src/main/java/com/viniweb/vproject/config/TestConfig.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.viniweb.vproject.entities.Category;
 import com.viniweb.vproject.entities.Order;
 import com.viniweb.vproject.entities.User;
 import com.viniweb.vproject.entities.enums.OrderStatus;
+import com.viniweb.vproject.repositories.CategoryRepository;
 import com.viniweb.vproject.repositories.OrderRepository;
 import com.viniweb.vproject.repositories.UserRepository;
 
@@ -24,8 +26,15 @@ public class TestConfig implements CommandLineRunner{
 	@Autowired
 	private OrderRepository orderRepository;
 	
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
+		
+		Category cat1 = new Category(null, "Electronics"); 
+		Category cat2 = new Category(null, "Books"); 
+		Category cat3 = new Category(null, "Swords");
 		
 		User u1 = new User(null, "Finn Mertens", "finn@gmail.com", "999999999", "123456"); 
 		User u2 = new User(null, "Jake the Dog", "jake@gmail.com", "999999992", "123456");
@@ -34,6 +43,8 @@ public class TestConfig implements CommandLineRunner{
 		Order o2 = new Order(null, Instant.parse("2021-03-22T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2); 
 		Order o3 = new Order(null, Instant.parse("2021-03-01T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
 		
+
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
 		
---
+++ src/main/java/com/viniweb/vproject/repositories/CategoryRepository.java
@@ -0,0 +1,10 @@
+package com.viniweb.vproject.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.viniweb.vproject.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long>{
+
+	
+}
