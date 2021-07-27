0066e2a3b0e749354e88ba650dc24a267e1a6840
viniyr <vinicius.yonezawa@gmail.com>
2021-07-26 23:25:14 -0300

    OrderStatus enum

--- src/main/java/com/viniweb/vproject/config/TestConfig.java
+++ src/main/java/com/viniweb/vproject/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.viniweb.vproject.entities.Order;
 import com.viniweb.vproject.entities.User;
+import com.viniweb.vproject.entities.enums.OrderStatus;
 import com.viniweb.vproject.repositories.OrderRepository;
 import com.viniweb.vproject.repositories.UserRepository;
 
@@ -29,9 +30,9 @@ public class TestConfig implements CommandLineRunner{
 		User u1 = new User(null, "Finn Mertens", "finn@gmail.com", "999999999", "123456"); 
 		User u2 = new User(null, "Jake the Dog", "jake@gmail.com", "999999992", "123456");
 		
-		Order o1 = new Order(null, Instant.parse("2021-04-04T19:53:07Z"), u1); 
-		Order o2 = new Order(null, Instant.parse("2021-03-22T03:42:10Z"), u2); 
-		Order o3 = new Order(null, Instant.parse("2021-03-01T15:21:22Z"), u1);
+		Order o1 = new Order(null, Instant.parse("2021-04-04T19:53:07Z"), OrderStatus.PAID, u1); 
+		Order o2 = new Order(null, Instant.parse("2021-03-22T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2); 
+		Order o3 = new Order(null, Instant.parse("2021-03-01T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
 		
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
