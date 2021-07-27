3eef7d0fd8a8126f5f997dfcfa05c3bcc3fcd505
viniyr <vinicius.yonezawa@gmail.com>
2021-07-27 00:36:42 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/viniweb/vproject/config/TestConfig.java
+++ src/main/java/com/viniweb/vproject/config/TestConfig.java
@@ -47,16 +47,25 @@ public class TestConfig implements CommandLineRunner{
 		Product p4 = new Product(null, "Finn Sword", "Donec aliquet odio ac rhoncus cursus.", 1200.0, ""); 
 		Product p5 = new Product(null, "Adventure time, Vol. 1", "Cras fringilla convallis sem vel faucibus.", 400.96, "");
 		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+		
+		p1.getCategories().add(cat3);
+		p2.getCategories().add(cat2);
+		p2.getCategories().add(cat3);
+		p3.getCategories().add(cat1);
+		p4.getCategories().add(cat3);
+		p5.getCategories().add(cat2);
+		
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+		
 		User u1 = new User(null, "Finn Mertens", "finn@gmail.com", "999999999", "123456"); 
 		User u2 = new User(null, "Jake the Dog", "jake@gmail.com", "999999992", "123456");
 		
 		Order o1 = new Order(null, Instant.parse("2021-04-04T19:53:07Z"), OrderStatus.PAID, u1); 
 		Order o2 = new Order(null, Instant.parse("2021-03-22T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2); 
 		Order o3 = new Order(null, Instant.parse("2021-03-01T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
-		
 
-		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
-		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
 		
