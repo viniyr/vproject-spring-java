a46d6e79b33a5850f246903b26512d72663b922b
viniyr <vinicius.yonezawa@gmail.com>
2021-07-27 23:39:36 -0300

    User insert

--- src/main/java/com/viniweb/vproject/entities/User.java
+++ src/main/java/com/viniweb/vproject/entities/User.java
@@ -14,7 +14,7 @@ import javax.persistence.Table;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
-@Table(name = "user")
+@Table(name = "tb_user")
 public class User implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
