0f5d59ceb7a34891bae53df926ff8344a14ced45
viniyr <vinicius.yonezawa@gmail.com>
2021-07-27 23:21:29 -0300

    Payment, one-to-one association

--- src/main/java/com/viniweb/vproject/config/TestConfig.java
+++ src/main/java/com/viniweb/vproject/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.viniweb.vproject.entities.Category;
 import com.viniweb.vproject.entities.Order;
 import com.viniweb.vproject.entities.OrderItem;
+import com.viniweb.vproject.entities.Payment;
 import com.viniweb.vproject.entities.Product;
 import com.viniweb.vproject.entities.User;
 import com.viniweb.vproject.entities.enums.OrderStatus;
@@ -80,6 +81,11 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2021-04-04T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 		
 	}
 	
