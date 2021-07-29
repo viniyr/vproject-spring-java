824b02211819ec99f6a806c948f8075adb244f1e
viniyr <vinicius.yonezawa@gmail.com>
2021-07-28 21:39:40 -0300

    Exception handling - findById

--- src/main/java/com/viniweb/vproject/services/UserService.java
+++ src/main/java/com/viniweb/vproject/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.viniweb.vproject.entities.User;
 import com.viniweb.vproject.repositories.UserRepository;
+import com.viniweb.vproject.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) { 
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) { 
